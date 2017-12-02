8a0a15b539f089212a8ff93f06fd4836fc397ac7
Satyendra-IITR <satyendrabanjare99@gmail.com>
2017-12-02 19:30:17 +0530

    update

--- Django/ProSearchServer/Query/models.py
+++ Django/ProSearchServer/Query/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+#from django.contrib.auth import User
 
-# Create your models here.
+class Query(models.Model):
+	username = models.TextField(max_length=20)
+   	Query = models.TextField(default="This is default query",null=False,max_length=1000)
+    #created = models.DateTimeField(auto_add_now=True)
+    #id = models.AutoField(primary_key=True,)
+
+    	#def __str__(self):
+    	#	return "%&" username
+
+    	
