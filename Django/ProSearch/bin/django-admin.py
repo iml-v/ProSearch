6298192b72258a18776a615aacd72ef8a204ea66
Satyendra-IITR <satyendrabanjare99@gmail.com>
2017-12-02 10:39:30 +0530

    Django_Initial_Commit

---
+++ Django/ProSearch/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/satyendra-banjare/Desktop/ProSearch/Django/ProSearch/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
