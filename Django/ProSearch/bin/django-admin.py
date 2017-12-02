e84e0b7017fbdb801679a767f6b7d5a1a871bf82
Satyendra-IITR <satyendrabanjare99@gmail.com>
2017-12-02 10:55:34 +0530

    initializing

--- Django/ProSearch/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/satyendra-banjare/Desktop/ProSearch/Django/ProSearch/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
